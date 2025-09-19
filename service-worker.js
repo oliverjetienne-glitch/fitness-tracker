7243a1faa1be986714e510b1bd42f3d07890e6ae
oliverjetienne-glitch <oliverjetienne@gmail.com>
2025-09-18 19:22:54 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+
+const CACHE = 'ft-cache-v311c';
+const FILES = ['index.html','style.css','app.js','manifest.json','icons/icon-192.png','icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
